be5ddfdc492372a37b4c83e4896353afc480df93
minalgk <minal.gk@gmail.com>
2018-11-05 23:00:59 +0000

    Delete test_data.sql

--- account-details/src/test/java/com/account/project/accountdetails/test_data.sql
+++
@@ -1,3 +0,0 @@
-insert into Account (firstname, lastname, accountnumber) values('John', 'Doe', '2234');
-insert into Account (firstname, lastname, accountnumber) values('Jane', 'Doe', '2235');
-insert into Account (firstname, lastname, accountnumber) values('Jim', 'Taylor', '2236');
